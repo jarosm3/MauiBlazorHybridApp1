c11868d9230c074268deecf61231a52a856db534
Miroslav Jaros <jarosm3@gmail.com>
2024-11-13 22:12:19 +0100

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiBlazorHybridApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
